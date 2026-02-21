9ceb74b4a2b30cb9e2f52bf29984707222d1bdad
Anton <baslykovanton3@gmail.com>
2026-02-21 23:26:08 +0300

    feat: add products full-text search

--- app/models/products.py
+++ app/models/products.py
@@ -1,6 +1,6 @@
-from datetime import datetime
 from decimal import Decimal
-from sqlalchemy import DateTime, String, Boolean, Integer, Numeric ,Float ,Text, func , text
+from sqlalchemy import String, Boolean, Integer, Numeric, Computed, Index
+from sqlalchemy.dialects.postgresql import TSVECTOR
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 
@@ -18,11 +18,24 @@ class Product(Base):
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
-    seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)  # New
-    rating: Mapped[float] = mapped_column(Float, default=0.0, nullable=False, server_default=text("0"))  # Средний рейтинг
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default=func.now(), onupdate=func.now(), nullable=False)
-
+    seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    
+    tsv: Mapped[TSVECTOR] = mapped_column(
+        TSVECTOR,
+        Computed(
+            """
+            setweight(to_tsvector('english', coalesce(name, '')), 'A')
+            || 
+            setweight(to_tsvector('english', coalesce(description, '')), 'B')
+            """,
+            persisted=True,
+        ),
+        nullable=False,
+    )
 
     category: Mapped["Category"] = relationship("Category", back_populates="products")
-    seller: Mapped["User"] = relationship("User", back_populates="products")  # New
+    seller: Mapped["User"] = relationship("User", back_populates="products")
+
+    __table_args__ = (
+        Index("ix_products_tsv_gin", "tsv", postgresql_using="gin"),
+    )
