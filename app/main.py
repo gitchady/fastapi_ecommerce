001fb33a144330e096783846ff5cf27468d5bddc
Anton <baslykovanton3@gmail.com>
2026-03-10 09:07:35 +0300

    Add request logging middleware

--- app/main.py
+++ app/main.py
@@ -8,6 +8,13 @@ from app.config import AUTO_CREATE_TABLES
 from app.database import Base, async_engine
 from app.routers import cart, categories, orders, payments, products, reviews, users
 
+from loguru import logger
+from uuid import uuid4
+from fastapi import Request
+from fastapi.responses import JSONResponse
+
+
+logger.add("info.log", format="Log: [{extra[log_id]}:{time} - {level} - {message}]", level="INFO", enqueue = True)
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
@@ -23,6 +30,22 @@ app = FastAPI(
     version="0.1.0",
     lifespan=lifespan,
 )
+
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
 
 app.include_router(categories.router)
 app.include_router(products.router)
